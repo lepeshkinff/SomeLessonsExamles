fb53aae1f6b9de64a1d4c9f956762860edb7eddc
lepeshkinff <lepeshkinff@gmail.com>
2022-10-15 14:10:42 +0300

    пример очереди в памяти

---
+++ QueueExample/ChessLibrary/ChessShape.cs
@@ -0,0 +1,52 @@
+﻿namespace ChessLibrary
+{
+	public abstract class ChessShape
+	{
+		private int _ShapeType;
+		public bool Color { get; set; }
+		public int ShapeType
+		{
+			get
+			{
+				return _ShapeType;
+			}
+			set
+			{
+				if (value < 0) return;
+				if (value > 100) return;
+				_ShapeType = value;
+			}
+		}
+
+
+		public abstract int[] GetMoves(int x);
+
+	}
+
+	public class ChessShapePawn : ChessShape
+	{
+		public override int[] GetMoves(int x)
+		{
+			if (ShapeType < 2)
+			{
+				return new[] { 1 * x, 2 * x };
+			}
+
+			if (ShapeType >= 2 && ShapeType < 10)
+			{
+				return new[] { 1 * x, 2 * x, 3 * x, 4 * x, 5 * x };
+			}
+
+			return new[] { 15 * x };
+		}
+	}
+
+	public class ChessShapeHorse : ChessShape
+	{
+		public override int[] GetMoves(int x)
+		{
+			return new[] { 1 * x, 2 * x };
+		}
+	}
+
+}
---
+++ QueueExample/QueueExample/Program.cs
@@ -0,0 +1,91 @@
+﻿using ChessLibrary;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+internal class Program
+{
+	private static int[] invoices = new int[1000];
+
+	private static async Task Main(string[] args)
+	{		
+		var cts = new CancellationTokenSource();
+		RunQueueProcessor(cts.Token);
+
+		var invoice = new int[1000];
+		var rnd = new Random();
+		for(var i = 0; i < invoice.Length; i++)
+		{
+			invoice[i] = rnd.Next();
+		}
+
+
+		var sw = new Stopwatch();
+		sw.Start();
+		var invoiceChaneRequest = rnd.Next();
+
+		for (var i = 0; i < invoice.Length; i++)
+		{
+			_queue.Enqueue(new MyTask
+			{
+				InvoiceIndex = i,
+				InvoiceChaneRequest = invoiceChaneRequest
+			});
+		}
+		sw.Stop();
+
+		Console.WriteLine(sw.Elapsed);
+
+		while(true)
+		{
+			var str = Console.ReadKey();
+			if(str.Key == ConsoleKey.Escape)
+			{
+				cts.Cancel();
+				break;
+			}
+
+			await Task.Delay(100);
+		}
+		WriteLine("Прекращено выполнение");
+		Console.ReadKey();
+	}
+
+	private static void WriteLine(string str)
+	{
+		lock("CONSOLE")
+		{
+			Console.WriteLine(str);
+		}
+	}
+
+	private static async Task RunQueueProcessor(CancellationToken cancellationToken)
+	{
+		while(!cancellationToken.IsCancellationRequested)
+		{
+			if(_queue.TryDequeue(out var queue))
+			{
+				ProcessItem(queue);
+			}
+			else
+			{
+				await Task.Delay(100);
+			}
+			WriteLine($"Queue is empty");			
+		}
+	}
+
+	private static async Task ProcessItem(MyTask item)
+	{
+		await Task.Yield();
+		WriteLine($"Process {item.InvoiceIndex} {item.InvoiceChaneRequest}");
+		invoices[item.InvoiceIndex] *= item.InvoiceChaneRequest;
+	}
+
+	private static ConcurrentQueue<MyTask> _queue = new ConcurrentQueue<MyTask>();
+}
+
+internal class MyTask
+{
+	public int InvoiceIndex { get; set; }
+	public int InvoiceChaneRequest { get; set; }
+}
