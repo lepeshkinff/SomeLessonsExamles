ff61eba7bbeca1fb86dc4dd2c2e4d859f0d5329d
lepeshkinff <lepeshkinff@gmail.com>
2022-10-15 15:00:10 +0300

    маленький сайтик для рендеринга

---
+++ RenderinoExamle/Renderino/Models/HomePageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Renderino.Models
+{
+	/// <summary>
+	/// Модель для главной страницы
+	/// </summary>
+	public record HomePageModel
+	{
+		public IEnumerable<string> Names { get; init; }
+	}
+}
