c5068afe537a673c59d1541126be71bf3b5a9b48
fedor.lepeshkin <fedor.lepeshkin@idp.zyfra.com>
2022-10-29 14:57:26 +0300

    client

---
+++ RenderinoExamle/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Client;
+namespace C1;
+
+public static class Programm
+{
+    public static async Task Main(string[] arg)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var client = new ApiClient(new HttpClient()
+        {
+            BaseAddress =new Uri("http://localhost:5012/")
+        });
+        var result = await client.GetNamesAsync(2);
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadKey();
+    }
+}
